ad34d0ffb9b09d67d9ba6e1da97cea9012f4b35b
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-04-04 23:32:13 +0100

    Implemented Moviedetails component

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState } from 'react';
+
+function MovieDetails() {
+
+    const [details, setDetails] = useState([
+        
+    ])
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MovieDetails;
