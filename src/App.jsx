cb0ba986443ad804c411e4ca11b84ee581bcccb5
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-04-05 21:32:12 +0100

    Rendered components and implemented routing on each component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer"
+import MovieList from "./components/MovieList";
+import HomePage from "./components/HomePage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MovieDetails from "./components/MovieDetails";
 
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/details/:id" element={<MovieDetails />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
   )
 }
 
