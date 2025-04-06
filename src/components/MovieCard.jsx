ee8ec67e5ae40ffa008f41acff927886b9b8f147
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-04-06 15:04:09 +0100

    Added a title to the search results

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,7 +6,7 @@ function MovieCard(props) {
 
   return (
     <div>
-        {props.movies &&<h2>Search Results</h2>}
+    <h2 className='text-xl text-gray-700 text-center bg-[#DA9F58]'>Search Results</h2>
     <div className='max-w-250 mx-auto grid grid-cols-2 sm:grid-cols-5'>
         {props.movies.map((movie, index) => (
             <div key={index} className='bg-gray-100 m-2 rounded-md overflow-hidden shadow-md max-w-75 transition-transform duration-200 hover:scale-105'>
