b30a6ff5fbc0c49b1ce8600bcd1264f15d07b882
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-04-08 22:58:59 +0100

    Decoupled the fetch function from the MovieCard component inorder to make the component more easier to manage

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
 
-function MovieCard(props) {
-
+function MovieCard({ movie, onClick }) {
 
   return (
-    <div>
-    <h2 className='text-xl text-gray-700 text-center bg-[#DA9F58]'>Search Results</h2>
-    <div className='max-w-250 mx-auto grid grid-cols-2 sm:grid-cols-5'>
-        {props.movies.map((movie, index) => (
-            <div key={index} className='bg-gray-100 m-2 rounded-md overflow-hidden shadow-md max-w-75 transition-transform duration-200 hover:scale-105'>
-                <Link to={`/details/${movie.imdbID}`}>
-                <img src={movie.Poster} alt="Movie Poster" />
-                <h2 className='text-lg font-bold text-gray-700 p-2'>{movie.Title}</h2>
-                <p className='text-sm p-2 text-gray-500'>{movie.Year}</p>
-                </Link>
-            </div>
-        ))}
-    </div>
+    <div 
+      className='bg-gray-100 m-2 rounded-md overflow-hidden shadow-md max-w-75 transition-transform duration-200 hover:scale-105'
+      onClick={onClick}
+    >
+        <img src={movie.Poster} alt="Movie Poster" />
+        <h2 className='text-lg font-bold text-gray-700 p-2'>{movie.Title}</h2>
+        <p className='text-sm p-2 text-gray-500'>{movie.Year}</p>
     </div>
   )
 }
