53278a61b1981adcde260841f9d6db61373ff03b
Tchatchouang95 <cedrickdjopgang@gmail.com>
2025-04-09 22:25:33 +0100

    Rendered all components to the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer"
-import MovieList from "./components/MovieList";
 import HomePage from "./components/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MovieDetails from "./components/MovieDetails";
 
 
 function App() {
@@ -11,13 +9,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/details/:id" element={<MovieDetails />} />
-          <Route path="/movie" element={<MovieList />} />
-        </Routes>
-      </Router>
+      <HomePage />
       <Footer />
     </div>
   )
